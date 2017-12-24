1e6cf56da9f8a567cfea3edad7deeef4690a766f
Tanvir Ahmed Khan <tk@ixora.com>
2017-12-24 17:19:23 +0600

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StudentApplicationFormComponent } from './student-application-form/student-application-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentApplicationFormComponent
   ],
   imports: [
     BrowserModule
